57fc26235f5483be9275e4ca45d4abfe0fe001ff
Andrew Pahom <Pahomov.kch@gmail.com>
2020-08-02 00:12:56 +0500

    Add @media and fix bug with draw components. It was problem with map key={}

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 // add input for change that variables
 const charAfterDot = 2
-const procentEveryYear = 0.04
+const procentEveryYear = 0.1
 
 
 function App() {
@@ -33,7 +33,7 @@ function App() {
   }
 
 //state
-  const [deposite, setDeposite] = useState(1000)
+  const [deposite, setDeposite] = useState(0)
   const [monthIncome, setMonthIncome] = useState(calculateProcentIncomeForMonth(deposite)) 
   const [yearIncome, setYearIncome] = useState(calculateForYear(deposite)) 
   const [addEveryMonth, setAddEveryMoth] = useState(0) 
@@ -46,66 +46,71 @@ function App() {
   }
   const summAddEveryMonthIsChanged = event => {
     setAddEveryMoth(+event.target.value)
+    setMonthIncome(calculateProcentIncomeForMonth(+event.target.value * 12))
     setYearIncome(calculateForYear(deposite, +event.target.value))
   }
 
 
+  const userDepositInfo = (
+    <div className={'variables'}>
+      <div className={'cell'}>
+        <p>Enter your deposit</p>
+        <input value={deposite} onChange={depositIsChange}  type={'text'}  />
+      </div>
+
+      <div className={'cell'}>
+        <p>% in Month</p> 
+        <input value={monthIncome} id={'inComeNextMonth'}  placeholder={'month'}  type={'text'}  />
+      </div>
+
+      <div className={'cell'}>
+        <p>% in Year</p> 
+        <input value={yearIncome} id={'inComeNextYear'}  placeholder={'year'}  type={'text'}  />
+      </div>
+
+      <div className={'cell'}>
+        <p>Add Monthly</p> 
+        <textarea placeholder={'Can add every month'} 
+                id={'addEveryMonth'}
+                value={addEveryMonth} 
+                onChange={summAddEveryMonthIsChanged} />
+      </div>
+    </div>)
+
+  const willBeMoneyOnAYear = calculateDepositeSizeForSomeMonths(deposite, addEveryMonth, 12)
+                                    .map( (sum, index) => {
+                                      let showSum = +sum.toFixed(charAfterDot)
+                                      return (
+                                        <div className={'calculateEachMonth'}>
+                                          <p>Your deposite after {index + 1} month:</p> 
+                                          <p className={'calculateDeposite'}>{showSum}</p>
+                                        </div>
+                                      )})  ;
+    
+    const willBeMoneyOnLongTherm = calculateDepositeSizeForSomeMonths(deposite, addEveryMonth, 240)
+                                      .map( (sum, index) => {
+                                        return (index === 59 || index === 119 || index === 179 || index === 239  ) ? 
+                                                <div className={'calculateEachMonth'}> 
+                                                    after {index=== 59 && '5 years'} 
+                                                          {index === 119 && '10 years'}
+                                                          {index === 179 && '15 years'}
+                                                          {index === 239 && '20 years'}
+                                                    <p className={'calculateDeposite'}>{sum}</p></div> 
+                                                : null 
+                                      })                                  
 
 
   return (
     <div className={'App'}>
-      {/* main summ */}
-      <div className={'variables'}>
-
-        <div className={'cell'}>
-          <p>Enter your deposit</p>
-          <input value={deposite} onChange={depositIsChange}  type={'text'}  />
+      <h1>Calculate your deposite</h1>
+      <div className={'container'}>
+        {userDepositInfo}
+        <div className={'willBe'}>
+          {willBeMoneyOnAYear}
         </div>
-
-        <div className={'cell'}>
-          <p>% in Month</p> 
-          <input value={monthIncome} id={'inComeNextMonth'}  placeholder={'month'}  type={'text'}  />
+        <div className={'longThermPlans'}>
+          {willBeMoneyOnLongTherm}
         </div>
-
-        <div className={'cell'}>
-          <p>% in Year</p> 
-          <input value={yearIncome} id={'inComeNextYear'}  placeholder={'year'}  type={'text'}  />
-        </div>
-
-        <div className={'cell'}>
-          <p>Add Monthly</p> 
-          <textarea placeholder={'Can add every month'} 
-                  id={'addEveryMonth'}
-                  value={addEveryMonth} 
-                  onChange={summAddEveryMonthIsChanged} />
-        </div>
-      </div>
-
-      <div className={'willBe'}>
-        {calculateDepositeSizeForSomeMonths(deposite, addEveryMonth, 12)
-          .map( (sum, index) => {
-            let showSum = +sum.toFixed(charAfterDot)
-            return (
-              <div key={sum} className={'calculateEachMonth'}>
-                <p>Your deposite after {index + 1} month:</p> <p className={'calculateDeposite'}>{showSum}</p>
-              </div>
-            )})}
-      </div>
-
-
-
-    
-      <div className={'longThermPlans'}>
-        {calculateDepositeSizeForSomeMonths(deposite, addEveryMonth, 240).map( (sum, index) => {
-          return (index === 59 || index === 119 || index === 179 || index === 239  ) ? 
-                  <div key={sum} className={'calculateEachMonth'}> 
-                      after {index=== 59 && '5 years'} 
-                            {index === 119 && '10 years'}
-                            {index === 179 && '15 years'}
-                            {index === 239 && '20 years'}
-                                  <p className={'calculateDeposite'}>{sum}</p></div> 
-                  : null 
-        })}
       </div>
     </div>)
 }
