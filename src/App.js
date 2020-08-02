caff723458f1a95e27fbc91d6b6245580a7acfac
Andrew Pahom <Pahomov.kch@gmail.com>
2020-08-03 00:40:32 +0500

    Add material UI on project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const procentEveryYear = 0.1
 
 
 function App() {
+  
 // Calculate
   const calculateProcentIncomeForMonth = (depositCount) => {
     return +((depositCount * procentEveryYear) / 12).toFixed(charAfterDot)
@@ -40,9 +41,13 @@ function App() {
 
 // EventListeners 
   const depositIsChange = event => {
-    setDeposite(+event.target.value)
-    setMonthIncome(calculateProcentIncomeForMonth(+event.target.value))
-    setYearIncome(calculateForYear(+event.target.value, addEveryMonth))
+    let newDepositSize = +event.target.value 
+    if (!newDepositSize || Number.isNaN(newDepositSize))  {
+      newDepositSize = 0
+    }
+    setDeposite(newDepositSize)
+    setMonthIncome(calculateProcentIncomeForMonth(newDepositSize))
+    setYearIncome(calculateForYear(newDepositSize, addEveryMonth))
   }
   const summAddEveryMonthIsChanged = event => {
     setAddEveryMoth(+event.target.value)
@@ -54,22 +59,22 @@ function App() {
   const userDepositInfo = (
     <div className={'variables'}>
       <div className={'cell'}>
-        <p>Enter your deposit</p>
+        <div>Enter your deposit</div>
         <input value={deposite} onChange={depositIsChange}  type={'text'}  />
       </div>
 
       <div className={'cell'}>
-        <p>% in Month</p> 
+        <div>% in Month</div> 
         <input value={monthIncome} id={'inComeNextMonth'}  placeholder={'month'}  type={'text'}  />
       </div>
 
       <div className={'cell'}>
-        <p>% in Year</p> 
+        <div>% in Year</div> 
         <input value={yearIncome} id={'inComeNextYear'}  placeholder={'year'}  type={'text'}  />
       </div>
 
       <div className={'cell'}>
-        <p>Add Monthly</p> 
+        <div>Add Monthly</div> 
         <textarea placeholder={'Can add every month'} 
                 id={'addEveryMonth'}
                 value={addEveryMonth} 
@@ -82,7 +87,7 @@ function App() {
                                       let showSum = +sum.toFixed(charAfterDot)
                                       return (
                                         <div className={'calculateEachMonth'}>
-                                          <p>Your deposite after {index + 1} month:</p> 
+                                          <p> After {index + 1} month:</p> 
                                           <p className={'calculateDeposite'}>{showSum}</p>
                                         </div>
                                       )})  ;
@@ -91,7 +96,7 @@ function App() {
                                       .map( (sum, index) => {
                                         return (index === 59 || index === 119 || index === 179 || index === 239  ) ? 
                                                 <div className={'calculateEachMonth'}> 
-                                                    after {index=== 59 && '5 years'} 
+                                                  After {index=== 59 && '5 years'} 
                                                           {index === 119 && '10 years'}
                                                           {index === 179 && '15 years'}
                                                           {index === 239 && '20 years'}
